13c3215de3192fe31e0259b31be36e5e4e225a5f
arkk200 <arkk20000@gmail.com>
2026-06-22 04:53:01 +0900

    useState 2

--- use-state.tsx
+++ use-state.tsx
@@ -1,34 +1,56 @@
-const stateList: { state: any }[] = [];
+let isPending = false;
+let curIndex = 0;
+
+const states: Record<number, { state: any; queue: any[] }> = {};
 
 type SetState<T> = (nextState: T | ((prevState: T) => T)) => void;
 
 const useState = <T,>(initialState: T | (() => T)): [T, SetState<T>] => {
-  const index = stateList.length;
-  if (initialState instanceof Function) {
-    stateList.push({
-      state: initialState(),
-    });
-  } else {
-    stateList.push({
-      state: initialState,
-    });
+  const index = curIndex++;
+
+  if (!Object.hasOwn(states, index)) {
+    if (initialState instanceof Function) {
+      states[index] = {
+        state: initialState(),
+        queue: [],
+      };
+    } else {
+      states[index] = {
+        state: initialState,
+        queue: [],
+      };
+    }
+  }
+
+  if (states[index].queue.length > 0) {
+    for (const job of states[index].queue) {
+      if (job instanceof Function) {
+        states[index] = {
+          ...states[index],
+          state: job(states[index].state),
+        };
+      } else {
+        states[index] = {
+          ...states[index],
+          state: job,
+        };
+      }
+    }
+
+    states[index].queue = [];
   }
 
   const setState: SetState<T> = (nextState) => {
-    queueMicrotask(() => {
-      if (nextState instanceof Function) {
-        stateList[index] = {
-          ...stateList[index],
-          state: nextState(stateList[index].state),
-        };
-      } else {
-        stateList[index] = {
-          ...stateList[index],
-          state: nextState,
-        };
-      }
-    });
+    states[index].queue.push(nextState);
+
+    if (!isPending) {
+      isPending = true;
+      queueMicrotask(() => {
+        isPending = false;
+        // render();
+      });
+    }
   };
 
-  return [stateList[index].state, setState];
+  return [states[index].state, setState];
 };
